26c46b17c98366efedc24ae7fc92a8505c961d46
Nikita <nksenofontov97@gmail.com>
2024-10-17 19:56:14 +0300

    added sout for method main

--- src/git_lesson/Main.java
+++ src/git_lesson/Main.java
@@ -1,9 +1,9 @@
 package git_lesson;
 
 public class Main {
-
+    // e48c88061125a67163e8ba1926196e4af5dfd9ea - hash commit (unique id for commit)
     public static void main(String[] args) {
-
+        System.out.println("Hello world");
     }
 
 }
