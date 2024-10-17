de035c38e0c5efc904542de51b8028c2685c1821
Nikita <nksenofontov97@gmail.com>
2024-10-17 20:19:44 +0300

    added in main new Persons modified Person.java

--- src/git_lesson/Main.java
+++ src/git_lesson/Main.java
@@ -1,10 +1,15 @@
 package git_lesson;
 
 public class Main {
-    // e48c88061125a67163e8ba1926196e4af5dfd9ea - hash commit (unique id for commit) - 1 commit
-    // 26c46b17c98366efedc24ae7fc92a8505c961d46 - second commit
+
     public static void main(String[] args) {
-        System.out.println("Hello world");
+
+        Person john = new Person("John","Smith");
+        Person barbara = new Person("Barbara","Sweps");
+
+
+        john.sayHello();
+        barbara.sayHello();
     }
 
 }
--- src/git_lesson/Person.java
+++ src/git_lesson/Person.java
@@ -1,4 +1,49 @@
 package git_lesson;
 
+import java.util.Objects;
+
 public class Person {
+
+
+    private String name;
+    private String surname;
+
+    public Person(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(name, person.name) &&
+                Objects.equals(surname, person.surname);
+    }
+
+    public void sayHello(){
+        System.out.println(name + " saying hello...");
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname);
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
 }
