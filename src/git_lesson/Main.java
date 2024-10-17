9295ce9dd236b3b630c2335897cd3f4f5e2c53b1
Nikita <nksenofontov97@gmail.com>
2024-10-17 19:58:54 +0300

    added new comment

--- src/git_lesson/Main.java
+++ src/git_lesson/Main.java
@@ -1,7 +1,8 @@
 package git_lesson;
 
 public class Main {
-    // e48c88061125a67163e8ba1926196e4af5dfd9ea - hash commit (unique id for commit)
+    // e48c88061125a67163e8ba1926196e4af5dfd9ea - hash commit (unique id for commit) - 1 commit
+    // 26c46b17c98366efedc24ae7fc92a8505c961d46 - second commit
     public static void main(String[] args) {
         System.out.println("Hello world");
     }
